e43d74127081c823037d549ce1bee08324dc775a
widevinedump <96489252+widevinedump@users.noreply.github.com>
2021-12-25 10:43:24 +0530

    New

---
+++ DOWNLOAD.cmd
@@ -0,0 +1,2 @@
+python bad37.py --url https://www.paramountplus.com/shows/mayor-of-kingstown/ -s 1 -e 3 --alang es-la en --slang es-la en --flang es-la
+pause
---
+++ pywidevine/clients/dictionary.py
@@ -0,0 +1,44 @@
+import re
+from unidecode import unidecode
+
+def get_release_tag(default_filename, vcodec, video_height, acodec, channels, bitrate, module, tag, isDual):
+    video_codec = ''
+
+    if 'avc' in vcodec:
+        video_codec = 'x264'
+    if 'hvc' in vcodec:
+        video_codec = 'x265'
+    elif 'dvh' in vcodec:
+        video_codec = 'HDR'
+    if isDual==True:
+        video_codec = video_codec + '.DUAL'
+        
+    audio_codec = ''
+    if 'mp4a' in acodec:
+        audio_codec = 'AAC'
+    if acodec == 'ac-3':
+        audio_codec = 'DD'
+    if acodec == 'ec-3':
+        audio_codec = 'DDP'
+    elif acodec == 'ec-3' and bitrate > 700000:
+        audio_codec = 'Atmos'
+
+    audio_channels = ''
+    if channels == '2':
+        audio_channels = '2.0'
+    elif channels == '6':
+        audio_channels = '5.1'
+    audio_format = audio_codec + audio_channels
+        
+    #if isDual==True:
+    #    audio_format = audio_codec + '.DUAL'
+    
+
+    default_filename = default_filename.replace('&', '.and.')
+    default_filename = re.sub(r'[]!"#$%\'()*+,:;<=>?@\\^_`{|}~[-]', '', default_filename)
+    default_filename = default_filename.replace(' ', '.')
+    default_filename = re.sub(r'\.{2,}', '.', default_filename)
+    default_filename = unidecode(default_filename)
+
+    output_name = '{}.{}p.{}.WEB-DL.{}.{}-{}'.format(default_filename, video_height, str(module), audio_format, video_codec, tag)
+    return output_name
